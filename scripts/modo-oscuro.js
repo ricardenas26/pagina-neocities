1b0231c6b2e7fca51eb5f68762f3146c598009cd
ricardenas26 <ricardenassanchez@gmail.com>
2026-03-15 22:06:08 -0600

    agregue un menu a todo aprovechando que tenia el js. asi que modifique el js y el libreria (se me habia olvidado mencionar que cree libreria, pero si, eso existe)

--- scripts/modo-oscuro.js
+++ scripts/modo-oscuro.js
@@ -1,3 +1,19 @@
+const menu = document.createElement("div");
+menu.id = "menu";
+menu.innerHTML = `
+  <p>
+    <a href="/index.html">menu</a> |
+    <a href="/apartados/discos/index.html">discos</a> |
+    <a href="/apartados/peliculas/index.html">peliculas</a> |
+    <a href="/apartados/libros/index.html">libros</a> |
+    <a href="/apartados/imagenes/index.html">imagenes</a> |
+    <a href="/apartados/blog/index.html">blog</a> |
+    <a href="/apartados/diario/index.html">diario</a> |
+    <a href="/https://www.last.fm/user/ricardenas26">last.fm</a>
+  </p>
+`;
+document.body.prepend(menu);
+
 const btn = document.createElement("button");
 btn.id = "boton-modo";
 document.body.prepend(btn);
